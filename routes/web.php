b6aad6afe7e86ed40ecedf1d040760a789cee341
frabaro <frabaro@hotmail.com>
2021-06-07 14:22:08 +0100

    ultimas alteracoes pag inicial

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
 Route::post('/save', [AuthController::class, 'save'])->name('auth.save');
